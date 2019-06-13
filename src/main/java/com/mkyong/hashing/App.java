ca2deda5daed186b44c0cc3bdaf8e5dbd8acb18a
pradeepkpal2011 <PradeepKPal2011@gmail.com>
2019-06-13 17:21:15 +0530

    secondcomit

--- src/main/java/com/mkyong/hashing/App.java
+++ src/main/java/com/mkyong/hashing/App.java
@@ -10,5 +10,7 @@ public class App
     {
         Evenodd ob= new Evenodd();
 System.out.println(ob.checkeven(Integer.parseInt(args[0])));
+System.out.println("Ok");
+System.out.println("Ok");
     }
 }
